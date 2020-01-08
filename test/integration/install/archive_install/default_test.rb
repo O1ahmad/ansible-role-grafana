98ef2f59d80f346c88982324e9038f2a40ce3954
Ahmad Wilson <ahwilson712@gmail.com>
2020-01-08 14:31:28 -0500

    add initial archive installation functionality

---
+++ test/integration/install/archive_install/default_test.rb
@@ -0,0 +1,16 @@
+title "Default role integrated test file"
+
+describe user('grafana') do
+  it { should exist }
+end
+
+describe group('grafana') do
+  it { should exist }
+end
+
+describe directory('/opt/grafana') do
+  it { should exist }
+  its('owner') { should eq 'grafana' }
+  its('group') { should eq 'grafana' }
+  its('mode') { should cmp '0755' }
+end
